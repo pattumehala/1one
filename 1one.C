dbcce7fcf5d62efe8170b03bc4fbb3a69f46831a
pattumehala <35220817+pattumehala@users.noreply.github.com>
2018-02-28 16:57:18 +0530

    1one.C
    print the word of a number

---
+++ 1one.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("\nEnter the number:");
+printf(%d,&num);
+switch(num)
+{
+case 1:printf("\nOne);
+       break;
+case 2:printf("\nTwo")
+       break;
+case 3:printf("\nThree");
+       break;
+case 4:printf("\nFour");
+       break;
+case 5:printf("\nFive");
+       break;
+case 6:printf("\nSix");
+       break;
+case 7:printf("\nSeven");
+       break;
+case 8:print("\nEight");
+       break;
+case 9:printf("\nNine");
+       break;
+case 10:printf("\nTen);
+        break;
+default:printf("\Invalid");
+        break;
+}
+return 0;
+}
